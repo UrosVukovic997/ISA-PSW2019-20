04fa1251f3509ea94c71e6f9ccbecc5a44d82614
Nikol-Kekic <nikola.get@gmail.com>
2019-12-07 12:51:30 +0100

    Proba

--- src/main/java/com/example/ClinicalCenter/controller/KartonController.java
+++ src/main/java/com/example/ClinicalCenter/controller/KartonController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.Column;
-
+//// Nikola grana
 public class KartonController {
 
         @Autowired
