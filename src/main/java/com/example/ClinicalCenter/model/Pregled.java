a4a1b1756c4d7f20115a325324fa26d50d722f88
Nikol-Kekic <nikola.get@gmail.com>
2020-02-02 14:24:44 +0100

    Front ( celokupna pocetna stranica lekara - sve osim kalendara ) Back (profil lekara, pregled pacijenata, 1/2 zahtevi za odmore/odsustva)

--- src/main/java/com/example/ClinicalCenter/model/Pregled.java
+++ src/main/java/com/example/ClinicalCenter/model/Pregled.java
@@ -16,8 +16,8 @@ import javax.persistence.OneToMany;
 
 
 @Entity
-
 public class Pregled {
+
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Long id;
@@ -34,7 +34,67 @@ public class Pregled {
     @Column(name = "jbo", nullable = false)
     private String jbo;
 
-    @Column(name = "date", nullable = false)
-    private Date termin;
+    @OneToMany(mappedBy = "pregled", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Termin termin;
 
+
+    public Pregled() {
+    }
+
+    public Pregled(Long id, String ime_pacijenta, String prezime_pacijenta, String email, String jbo) {
+        this.id = id;
+        this.ime_pacijenta = ime_pacijenta;
+        this.prezime_pacijenta = prezime_pacijenta;
+        this.email = email;
+        this.jbo = jbo;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIme_pacijenta() {
+        return ime_pacijenta;
+    }
+
+    public void setIme_pacijenta(String ime_pacijenta) {
+        this.ime_pacijenta = ime_pacijenta;
+    }
+
+    public String getPrezime_pacijenta() {
+        return prezime_pacijenta;
+    }
+
+    public void setPrezime_pacijenta(String prezime_pacijenta) {
+        this.prezime_pacijenta = prezime_pacijenta;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJbo() {
+        return jbo;
+    }
+
+    public void setJbo(String jbo) {
+        this.jbo = jbo;
+    }
+
+    public Termin getTermin() {
+        return termin;
+    }
+
+    public void setTermin(Termin termin) {
+        this.termin = termin;
+    }
 }
